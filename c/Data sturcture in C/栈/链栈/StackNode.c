9baa9bcc15377315f6317eee24c7c9387d8d4eb2
_wander <390202863@qq.com>
2026-07-02 22:06:05 +0800

    获取栈顶元素、栈中元素个数、销毁链栈

--- c/Data sturcture in C/栈/链栈/StackNode.c
+++ c/Data sturcture in C/栈/链栈/StackNode.c
@@ -27,11 +27,45 @@ int PushStack(StackNode *S,SElemType e){
     S->next = p;       //让头节点指向新节点 p
     return 1;          //顺序不能乱
 }
-//4.出栈(出栈) 删除头结点后的第一个节点
+//4.出栈(弹栈) 删除头结点后的第一个节点
 bool Pop(LinkStack S,SElemType *e){
     if(StackEmpty(S)){
         prinf("栈空,无法出栈\n");
         return false;
     }
+    SNode *p=S->next;
+    *e = p->data;
+    S->next = p->next;
+    free(p);
+    return true;
+}
+//5.获取栈顶元素
+SElemType GetTop(StackNode *S){
+    if(S->next!=NULL){
+        return S->next->data;
+    }    
+}
+//获取栈中元素个数
+int StackLength(LinkStack S) {
+    int len = 0;
+    StackNode *p = S->next;
+    while (p) {
+        len++;
+        p = p->next;
+    }
+    return len;
+}
+//销毁链栈（释放所有节点）
+void DestroyStack(LinkStack *S) {
+    StackNode *p = *S;
+    StackNode *q;
+    while (p) {
+        q = p->next;
+        free(p);
+        p = q;
+    }
+    *S = NULL;  // 防止野指针
+}
+int main(){
     
 }
